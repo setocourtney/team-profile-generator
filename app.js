6992644fe90ed96b1dd080bfa36d0916becdf61e
setocourtney <courtneyjseto@gmail.com>
2020-02-29 17:07:51 -0800

    added prompts for employee info to app.js, create employee objects and add to employee array

--- app.js
+++ app.js
@@ -4,32 +4,104 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-​
-const OUTPUT_DIR = path.resolve(__dirname, "output")
+
+const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
-​
 const render = require("./lib/htmlRenderer");
-​
-​
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-​
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-​
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-​
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-​
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an 
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work!```
+
+let employees = [];
+
+const getEmployee = () => {
+    inquirer.prompt([
+        {
+            name: "name",
+            type: "input",
+            message: "What is the Employee\'s Name?"
+        },
+        {
+            name: "role",
+            type: "list",
+            message: "What is the Employee\'s current role?",
+            choices: ["Engineer", "Intern", "Manager"]
+        },
+        {
+            name: "email",
+            type: "input",
+            message: "What is the Employee\'s email?"
+        }
+    ]).then((answer) => {
+        const employee = {
+            name: answer.name,
+            role: answer.role,
+            email: answer.email
+        };
+        switch (answer.role) {
+            case "Engineer":
+                createEngineer(employee);
+                break;
+            case "Intern":
+                createIntern(employee);
+                break;
+            case "Manager":
+                createManager(employee);
+                break;
+            default:
+                break;
+        };
+    });
+};
+
+getEmployee();
+
+const askAgain = () => {
+    inquirer.prompt({
+        name: "continue",
+        type: "confirm",
+        message: "Add another employee?"
+    }).then((answer) => {
+        if(answer.continue) {
+            getEmployee();
+        } else {
+            return;
+        }
+    });
+};
+
+const createEngineer = (employee) => {
+    inquirer.prompt({
+        name: "github",
+        type: "input",
+        message: "What is the Employee\'s GitHub Profile?"
+    }).then((answer) => {
+        const engr = new Engineer(employee.name, employees.length + 1, employee.email, answer.github);
+        employees.push(engr);
+        console.log(employees);
+        askAgain();
+    });
+};
+
+const createIntern = (employee) => {
+    inquirer.prompt({
+        name: "school",
+        type: "input",
+        message: "Where does the Employee attend school?"
+    }).then((answer) => {
+        const newIntern= new Intern(employee.name, employees.length + 1, employee.email, answer.school);
+        employees.push(newIntern);
+        console.log(employees);
+        askAgain();
+    });
+};
+
+const createManager = (employee) => {
+    inquirer.prompt({
+        name: "office",
+        type: "input",
+        message: "What is the Employee\'s office number?"
+    }).then((answer) => {
+        const mgr= new Manager(employee.name, employees.length + 1, employee.email, answer.office);
+        employees.push(mgr);
+        console.log(employees);
+        askAgain();
+    });
+};
